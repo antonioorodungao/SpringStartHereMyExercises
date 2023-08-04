68a428c5ac723c65184d1169e4a2bf23407a6035
Antonio Oro Dungao <antonioorodungao@yahoo.com>
2023-08-04 08:23:52 +0300

    Completed Chapter 2 of Spring Start Here Book

--- ch2.springcontext/src/main/java/org/example/Main.java
+++ ch2.springcontext/src/main/java/org/example/Main.java
@@ -1,19 +1,26 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Parrot p = context.getBean("parrot",Parrot.class);
+        System.out.println(p);
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        Parrot p2 = context.getBean("parrot2",Parrot.class);
+        System.out.println(p2);
+
+
+        Parrot p_default = context.getBean(Parrot.class);
+        System.out.println("default: " +  p_default);
+
+        String hello = context.getBean("hello", String.class);
+        System.out.println(hello);
+
+        Integer ten = context.getBean("ten", Integer.class);
+        System.out.println(ten);
+
     }
 }
